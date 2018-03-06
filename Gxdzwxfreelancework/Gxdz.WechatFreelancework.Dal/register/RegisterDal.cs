66ed2c9f2c336dee7bc6f1a6c2c6af5c2cd18d42
ycab <1210626681@qq.com>
2018-03-06 21:47:07 +0800

    初步建立框架

---
+++ Gxdzwxfreelancework/Gxdz.WechatFreelancework.Dal/register/RegisterDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.OracleClient;
+
+
+namespace Gxdz.WechatFreelancework.Dal.register
+{
+    class RegisterDal
+    {
+    }
+}

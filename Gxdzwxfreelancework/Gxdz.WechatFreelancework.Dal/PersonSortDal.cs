d9ba88e6d170f32e69cf686ff9da1a1e836ca4a4
ycab <1210626681@qq.com>
2018-03-21 20:05:19 +0800

    成功完成分类的js编写

--- Gxdzwxfreelancework/Gxdz.WechatFreelancework.Dal/PersonSortDal.cs
+++ Gxdzwxfreelancework/Gxdz.WechatFreelancework.Dal/PersonSortDal.cs
@@ -33,12 +33,28 @@ namespace Gxdz.WechatFreelancework.Dal
         public string MiddleSortNameInfo()
         {
             string responseText = "";
-            string sql = "select * from GXFW_INFO";
+            string sql = "select * from GXFW_PROFESSION_CATEGORY";
             DataTable dt = OracleHelper.GetTable(sql, null);
             if (dt.Rows.Count != 0)
             {
-                responseText = JsonHelper.getRecordJson(dt);
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    string profession_name = dt.Rows[i]["PROFESSION_NAME"].ToString();
+                    string profession_id = dt.Rows[i]["PROFESSION_ID"].ToString();
+                    string sql1 = string.Format("select * from GXFW_INFO t where PROFESSION='{0}' ", profession_name);
+                    DataTable dt1 = OracleHelper.GetTable(sql1, null);
+                    string response = JsonHelper.getRecordJson(dt1);
+                    if (i == dt.Rows.Count - 1)
+                    {
+                        responseText += "{\"profession_name\":\"" + profession_name + "\",\"profession_id\":\"" + profession_id + "\",\"info\":[" + response + "]}";
+                    }
+                    else
+                    {
+                        responseText += "{\"profession_name\":\"" + profession_name + "\",\"profession_id\":\"" + profession_id + "\",\"info\":[" + response + "]},";
+                    }
+                }
                 responseText = "{\"msg\":\"success\",\"middlesortinfo\":[" + responseText + "]}";
+           
             }
             else
             {
